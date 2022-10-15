f6136b8f7bd56ec32ee61fb4229acdbf5f4ea2da
ErckLima <erickfalubay@gmail.com>
2022-10-15 17:03:51 -0300

    tete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const senha_lucas = "Lucas123"
 function go(){
     var emailvalidação = document.getElementById("Email").value;
     var senhavalidação = document.getElementById("senha").value;
-    
+
 
     if(emailvalidação == email_erick && senhavalidação == senha_erick){
         location.href = "home.html";
@@ -20,3 +20,5 @@ function go(){
         window.alert("Senha ou E-mail incorreto...")
     )
 }
+
+//gasdasdasiusd
