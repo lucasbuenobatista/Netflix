3dfbc095d7c674155c5d0002a895b92e7326419a
ErckLima <erickfalubay@gmail.com>
2022-10-15 19:26:26 -0300

    Integração da api

--- api.js
+++ api.js
@@ -1,3 +1,55 @@
+function Pesquisa(e){
+    e.preventDefault();
+
+    let filme = document.querySelector('.TextoPesquisar').value;
+
+    if(filme){
+        const _url = `https://www.omdbapi.com/?s=${filme}&apikey=cb976d7c`;
+        const _opções = {
+            method: 'GET',
+            mode: 'cors',
+            redirect: 'follow',
+            cache: 'default'
+        }
+
+        fetch(_url, _opções)
+            .then(function(response){
+                //tratamento do erro
+
+                if(!response.ok) throw new Error['Erro ao executar requisição']
+
+                //retorno do objeto .json
+                return response.json();
+
+            })
+
+            .then(function(data){
+                console.log(data);
+                let conteudo = '';
+                for(let i = 1; i < data.Search.length; i++){
+                    conteudo += `<li class="films">`;
+                    conteudo += `<figure class="figuras">`;
+                    conteudo += `<img class="img" src="${data.Search[i].Poster}"/>`;
+                    conteudo += `</figure>`;
+                    conteudo += `<legend class="legenda">`;
+                    conteudo += `<span class="ano">${data.Search[i].Year}</span>`;
+                    conteudo += `<h2 class="titulo">${data.Search[i].Title}</h2>`;
+                    conteudo += `</legend>`;
+                    conteudo += `</li>`;
+
+
+                }
+
+                document.getElementById("Filmes").innerHTML = conteudo
+
+            })
+
+    }else{
+        alert("Digite um filme!")
+    }
+
+
+}
 
 
 
@@ -5,5 +57,6 @@
 
 
 window.onload = () => {
-    const submit = document.queri
+    const submit = document.querySelector('.BotaoPesquisar');
+    submit.addEventListener('click', Pesquisa);
 }
