132074f19294f1ab5f1867fca0961345d23769ad
Erick Lima <110945352+ErckLima@users.noreply.github.com>
2023-03-09 21:47:59 -0300

    testetstets

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const senha_erick = "Erick123"
 const email_lucas = "Lucas@Metflix.com.br"
 const senha_lucas = "Lucas123"
 
-function go(e){
-    e.preventDefault();
+function go{
     var emailvalidação = document.getElementById("Email").value;
     var senhavalidação = document.getElementById("senha").value;
 
